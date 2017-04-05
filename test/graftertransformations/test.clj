5f0a887e4f0cc41976f597ca252d0bc930a80dca
xaabi6 <xabierportu@gmail.com>
2017-04-05 14:04:29 +0200

    Created the first test
    Let's start creating tests for checking our data quality ;)

---
+++ test/graftertransformations/test.clj
@@ -0,0 +1,16 @@
+(ns graftertransformations.test
+  (:require
+   [clojure.test :refer :all]
+   [graftertransformations.pipeline :refer :all]
+  )
+)
+
+(deftest test-celica-brand
+  (testing "Value of brand column for row 1 should be Toyota"
+     (is (= "Toyota"
+            ; There is probably a better solution to achieve this
+            (get (nth (:rows (convert-celica-to-data "./data/celica-data-test.csv")) 1) "brand" )
+         )
+     )
+  )
+)
