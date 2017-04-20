2b537ab4fbd05c0af923816ba08fd176e4cc2228
xaabi6 <xabierportu@gmail.com>
2017-04-20 12:11:04 +0200

    Fix tests rows names
    In the last commit I refactored some columns from 'celica-data.csv' and
    I forgot to change row names

--- test/graftertransformations/test.clj
+++ test/graftertransformations/test.clj
@@ -6,21 +6,21 @@
   )
 )
 
-(deftest test-celica-brand
-  (testing "Value of brand column for row 0 should be Toyota"
+(deftest test-celica-manufacturer
+  (testing "Value of manufacturer column for row 0 should be Toyota"
      (is (= "Toyota"
             ; There is probably a better solution to achieve this
-            (get (nth (:rows (convert-celica-to-data "./data/celica-data.csv")) 0) :brand )
+            (get (nth (:rows (convert-celica-to-data "./data/celica-data.csv")) 0) :manufacturer )
          )
      )
   )
 )
 
-(deftest test-celica-name
-  (testing "Value of name column for row 1 should be Celica"
+(deftest test-celica-model
+  (testing "Value of model column for row 1 should be Celica"
      (is (= "Celica"
             ; There is probably a better solution to achieve this
-            (get (nth (:rows (convert-celica-to-data "./data/celica-data.csv")) 1) :name )
+            (get (nth (:rows (convert-celica-to-data "./data/celica-data.csv")) 1) :model )
          )
      )
   )
